9e023aed0c41a73f243d942cb042af0171c3922a
madalinagomei <150043594+madalinagomei@users.noreply.github.com>
2024-02-17 15:41:36 +0000

    Deploying to gh-pages from @ madalinagomei/team9-project@fbc1cf4472efed9a03b08a8fa2c2e3408361de61 🚀

---
+++ index.4f42f105.js
@@ -0,0 +1,2 @@
+!function(){var e;(e={openModalBtn:document.querySelector("[data-modal-open-mobile-buy]"),closeModalBtn:document.querySelector("[data-modal-close-mobile-buy]"),modal:document.querySelector("[data-modal-mobile-buy]")}).openModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden-buy")}))}();
+//# sourceMappingURL=index.4f42f105.js.map
